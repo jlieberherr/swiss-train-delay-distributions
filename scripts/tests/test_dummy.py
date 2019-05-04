8580d12311d75953d438d6b867f152667dafaae9
johanneslieberherr <johannes.lieberherr@ttools.ch>
2019-05-04 19:14:27 +0200

    travis added

--- scripts/tests/test_dummy.py
+++ scripts/tests/test_dummy.py
@@ -6,7 +6,7 @@ import unittest
 
 class TestCase(unittest.TestCase):
     def test_something(self):
-        self.assertEqual(True, False)
+        self.assertTrue(True)
 
 
 if __name__ == '__main__':
