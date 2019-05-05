7e0ed9d526beb396db83cba8d8c138cc7993dfb0
johanneslieberherr <johannes.lieberherr@ttools.ch>
2019-05-05 12:15:40 +0200

    cleanup project setup

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import logging
 import unittest
 
 from scripts.preprocessing import run_preprocessing
-
-log = logging.getLogger(__name__)
 
 
 class TestCase(unittest.TestCase):
